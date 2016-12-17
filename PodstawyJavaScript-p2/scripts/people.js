c76eb9189e226a28864e48f0c27e2fe3b0acc3b4
Paweł Kaczmarek <pawel.kaczmarek.85@gmail.com>
2016-12-17 13:39:31 +0100

    podstawy JS - materiały

---
+++ PodstawyJavaScript-p2/scripts/people.js
@@ -0,0 +1,138 @@
+var people = [
+	{
+		"firstName": "Rasmussen",
+		"lastName": "Riddle",
+		"gender": "male",
+		"age": 31,
+		"company": "ORBAXTER",
+		"email": "rasmussenriddle@orbaxter.com"
+	},
+	{
+		"firstName": "Strickland",
+		"lastName": "Gallagher",
+		"gender": "male",
+		"age": 40,
+		"company": "CORECOM",
+		"email": "stricklandgallagher@corecom.com"
+	},
+	{
+		"firstName": "Leticia",
+		"lastName": "Olson",
+		"gender": "female",
+		"age": 24,
+		"company": "SQUISH",
+		"email": "leticiaolson@squish.com"
+	},
+	{
+		"firstName": "Rogers",
+		"lastName": "Kim",
+		"gender": "male",
+		"age": 29,
+		"company": "SKYBOLD",
+		"email": "rogerskim@skybold.com"
+	},
+	{
+		"firstName": "Rita",
+		"lastName": "Holloway",
+		"gender": "female",
+		"age": 30,
+		"company": "CABLAM",
+		"email": "ritaholloway@cablam.com"
+	},
+	{
+		"firstName": "Matilda",
+		"lastName": "Byrd",
+		"gender": "female",
+		"age": 31,
+		"company": "JUMPSTACK",
+		"email": "matildabyrd@jumpstack.com"
+	},
+	{
+		"firstName": "Kerr",
+		"lastName": "Peters",
+		"gender": "male",
+		"age": 35,
+		"company": "NORALI",
+		"email": "kerrpeters@norali.com"
+	},
+	{
+		"firstName": "Meadows",
+		"lastName": "Byers",
+		"gender": "male",
+		"age": 33,
+		"company": "INSOURCE",
+		"email": "meadowsbyers@insource.com"
+	},
+	{
+		"firstName": "Leigh",
+		"lastName": "Dyer",
+		"gender": "female",
+		"age": 36,
+		"company": "CALCU",
+		"email": "leighdyer@calcu.com"
+	},
+	{
+		"firstName": "Sharp",
+		"lastName": "Fitzpatrick",
+		"gender": "male",
+		"age": 23,
+		"company": "PYRAMI",
+		"email": "sharpfitzpatrick@pyrami.com"
+	},
+	{
+		"firstName": "Goodwin",
+		"lastName": "Woodward",
+		"gender": "male",
+		"age": 34,
+		"company": "SCENTY",
+		"email": "goodwinwoodward@scenty.com"
+	},
+	{
+		"firstName": "Esperanza",
+		"lastName": "Jenkins",
+		"gender": "female",
+		"age": 40,
+		"company": "FUTURIS",
+		"email": "esperanzajenkins@futuris.com"
+	},
+	{
+		"firstName": "Velez",
+		"lastName": "Lucas",
+		"gender": "male",
+		"age": 23,
+		"company": "HONOTRON",
+		"email": "velezlucas@honotron.com"
+	},
+	{
+		"firstName": "White",
+		"lastName": "Blackwell",
+		"gender": "male",
+		"age": 26,
+		"company": "TURNLING",
+		"email": "whiteblackwell@turnling.com"
+	},
+	{
+		"firstName": "Anna",
+		"lastName": "Schultz",
+		"gender": "female",
+		"age": 28,
+		"company": "STEELFAB",
+		"email": "annaschultz@steelfab.com"
+	},
+	{
+		"firstName": "Morton",
+		"lastName": "Benson",
+		"gender": "male",
+		"age": 32,
+		"company": "PLASMOS",
+		"email": "mortonbenson@plasmos.com"
+	},
+	{
+		"firstName": "Audra",
+		"lastName": "Odonnell",
+		"gender": "female",
+		"age": 32,
+		"company": "VISUALIX",
+		"email": "audraodonnell@visualix.com"
+	}
+];
